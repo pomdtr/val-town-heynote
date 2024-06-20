24359527223794420131a632f7d265169223fe99
pomdtr <achille.lacoin@gmail.com>
2024-06-20 22:08:22 +0200

    add back the files

---
+++ embed/static/site.webmanifest_.ts
@@ -0,0 +1,8 @@
+import {F} from "jsr:@nfnitloop/deno-embedder@1.3.1/embed.ts"
+export default F({
+ size: 189,
+ encoded: `
+ewogICAgIm5hbWUiOiAiSGV5bm90ZSIsCiAgICAic2hvcnRfbmFtZSI6ICJIZXlub3RlIiwKICAgICJpY29ucyI6IFsKICAgICAgICB7CiAgICAgICAgICAg
+ICJzcmMiOiAiL2ljb24uaWNvIiwKICAgICAgICAgICAgInNpemVzIjogIjI1NngyNTYiCiAgICAgICAgfQogICAgXSwKICAgICJkaXNwbGF5IjogInN0YW5k
+YWxvbmUiCn0K`,
+})
